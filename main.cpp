69b61498bc3ef4868e38636c485dd09e3177d828
Jashandeep Singh <jashandeep@8chili.com>
2022-12-23 12:56:18 +0530

    main.cpp setup

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	// START
+	
+	int x = 34;
+	int y = 33;
+
+	cout << y - x << endl;
+
+	// END
+
+	return 0;
+}
