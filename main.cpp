d03147c02c69431887f278e5879d7a6ab718b90f
Jashandeep Singh <jashanbhullar376@gmail.com>
2023-01-25 17:18:26 +0530

    size of binary representation without leading zeros

--- main.cpp
+++ main.cpp
@@ -2,15 +2,29 @@
 using namespace std;
 
 
+
+void bin(int x)
+{
+	bitset<16> xbit(x);
+	cout << xbit << endl;
+
+	string ss = "";
+	while(x > 0)
+	{
+		ss = (x&1 ? "1" : "0") + ss;
+		x = x >> 1;
+	}
+
+	cout << ss;
+}
+
 void mainFunc()
 {
-	unsigned int x = 2;
 
-	bitset<8> xBit(x);
 
-	xBit = xBit << 2;
-
-	cout << xBit << endl;
+	long long int x = 1;
+	cout << bitset<32>(x) << endl;
+	cout << (int)log2(x);
 }
 
 int main()
